c7db356855a79015ec0a62acf7ca02b2e3334e2b
Jack Lam <catinacan@gmail.com>
2016-05-09 16:55:01 -1000

    Added support for two players.

--- Lightsaber/Assets/MarksmanHController.cs
+++ Lightsaber/Assets/MarksmanHController.cs
@@ -5,6 +5,7 @@ public class MarksmanHController : MonoBehaviour {
     public GameObject Laser_Emitter;
     public GameObject Laser;
     public float Laser_Forward_force;
+	private float timer = 0.1f;
 
     // Use this for initialization
     void Start() {
@@ -19,14 +20,19 @@ public class MarksmanHController : MonoBehaviour {
     }
 
     public void fireLaser() {
-        GameObject Temporary_Bullet_Handler;
-        Temporary_Bullet_Handler = Instantiate(Laser, Laser_Emitter.transform.position, Laser_Emitter.transform.rotation) as GameObject;
+		if (timer < 0) {	
+			GameObject Temporary_Bullet_Handler;
+			Temporary_Bullet_Handler = Instantiate (Laser, Laser_Emitter.transform.position, Laser_Emitter.transform.rotation) as GameObject;
 
-        Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+			Temporary_Bullet_Handler.transform.Rotate (Vector3.left * 90);
 
-        Rigidbody temp_rigidbody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
-        temp_rigidbody.velocity = -1 * transform.forward * Laser_Forward_force;
+			Rigidbody temp_rigidbody = Temporary_Bullet_Handler.GetComponent<Rigidbody> ();
+			temp_rigidbody.velocity = 1
+				* transform.forward * Laser_Forward_force;
 
-        Destroy(Temporary_Bullet_Handler, 10.0f);
+			Destroy (Temporary_Bullet_Handler, 10.0f);
+			timer = 0.1f;
+		} else
+			timer = timer - Time.deltaTime;
     }
 }
